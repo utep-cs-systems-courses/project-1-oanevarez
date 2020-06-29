394524d4e403c57a8d85616b85e48bd096713fa9
Osvaldo Asael Nevarez <osvaldonevarez@osvaldos-mbp.home>
2020-06-29 00:13:47 -0600

    Worked on tokenizer.h

---
+++ src/TokenizerInterface.c
@@ -0,0 +1,35 @@
+//
+//  TokenizerInterface.c
+//  
+//
+//  Created by Osvaldo Asael Nevarez  on 6/28/20.
+//
+
+#include <stdio.h>
+#include "tokenizer.h"
+
+int main(){
+    
+    char str [1000];
+    
+    printf("Please enter a sentance to tokenize... I will echo\n");
+    fgets(str, 1000, stdin);
+    printf("Echoing ... \n %s\n", str);
+    
+    printf("\n");
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+    
+    
+    
+    
+    return 0;
+}
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,32 +1,121 @@
 #ifndef _TOKENIZER_
 #define _TOKENIZER_
 
+//null char is a string terminator
 
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c){
     
+    int i = 0; //0 is false 1 is true
+    
+    //\v is a verticle tab
+    if(c == '\0' || c == ' ' || c == '\t' || c == '\v' || c == '.'){
+        i = 1; //true
+        return i;
+    }
+    
+    return 0;
+    
+}
+    
+
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c){
+    
+    
+    int i = 0; //0 is false 1 is true
+     
+     //\v is a verticle tab
+     if(c != '\0' || c != ' ' || c != '\t' || c != '\v' || c != '.'){
+         i = 1; //true
+         return i;
+     }
+     
+     return 0;
+    
+    
+}
+
+
+
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char word_start(char *str); 
+char *word_start(char *str){
+
+    while(space_char(*str)){
+        if(non_space_char(*str)){
+            return str;
+        }
+        str++;
+    }
+    return 0;
+}
+
+
+
+
 
 /* Returns a pointer terminator char following *word */
-char *word_terminator(char *word);
+char *word_terminator(char *word){
+   
+    while(non_space_char(*word)){
+        //keeps going until finds a terminator
+        if(space_char(*word)){
+            //if theres a terminator returns word pt
+            return word;
+        }
+
+        word++;
+    }
+    
+    return 0;
+}
+    
+
 
 /* Counts the number of words in the string argument. */
-int count_words(char *str);
+int count_words(char *str){
+    
+    int totalNum=0;
+    char theWord ='w'; // w means word
+    
+    while(non_space_char(*str)){
+        
+        if(space_char(*str)){
+            totalNum ++;
+        }
+        
+        str++;
+    }
+    
+    
+    return totalNum;
+}
+
+
+
 
 /* Returns a fresly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
 char *copy_str(char *inStr, short len);
+
+
+
+
+
+
+
+
+
+
+
 
 /* Returns a freshly allocated zero-terminated vector of freshly allocated 
    space-separated tokens from zero-terminated str.
@@ -39,10 +128,32 @@ char *copy_str(char *inStr, short len);
 */
 char **tokenize(char* str);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Prints all tokens. */
-void print_tokens(char **tokens);
+void print_tokens(char **tokens){
+    
+    
+}
+
+
+
 
 /* Frees all tokens and the vector containing themx. */
-void free_tokens(char **tokens);
+void free_tokens(char **tokens){
+ 
+}
+
 
 #endif
